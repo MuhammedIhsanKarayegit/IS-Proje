e19f17667cf41ad5338c05ef7fc2fe3b8b372b5f
MuhammedIhsanKarayegit <104142526+MuhammedIhsanKarayegit@users.noreply.github.com>
2023-12-31 23:59:03 +0300

    Update Colors.java

--- 48.grup/Java-Host-Dispatch-master/src/com/company/Colors.java
+++ 48.grup/Java-Host-Dispatch-master/src/com/company/Colors.java
@@ -2,7 +2,7 @@
 package com.company;
 
 import java.util.ArrayList;
-
+//Renkler
 public class Colors {
     public ArrayList<String> getAllColors() {
         ArrayList<String> colors = new ArrayList<>();
